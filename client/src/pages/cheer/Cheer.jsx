4b29efdfa3c7ff6413144d6e36deee35da3e42dd
gaeun <geun5744@gmail.com>
2022-11-26 00:10:05 +0900

    UPDATE Cheer.jsx, Map.jsx 틀 구성

---
+++ client/src/pages/cheer/Cheer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Cheer() {
+  return <div>응원하기 페이지</div>;
+}
---
+++ client/src/pages/map/Map.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function map() {
+  return <div>직관 꿀팁</div>;
+}
