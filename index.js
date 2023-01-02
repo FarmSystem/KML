70da688ef1ac7f50a784546977fe1ba44d464030
gaeun <geun5744@gmail.com>
2023-01-03 01:33:21 +0900

    응원메시지 몽구스 스키마 설정 및 router 연결

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
+const port = 5000;
+
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const messageRoute = require("./routes/message");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-app.listen("5000", () => {
+app.use("/cheer", messageRoute);
+
+app.use("/api/auth", authRoute);
+
+app.listen(port, () => {
   console.log("Backend is running");
 });
---
+++ routes/message.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const Message = require("../models/Message");
+
+// 메시지 적기
+router.post("/", async (req, res) => {
+  try {
+    const newMessage = new Message({
+      nickName: req.body.nickName,
+      message: req.body.message,
+    });
+    const savedMessage = await newMessage.save();
+    res.status(200).json(savedMessage);
+  } catch {
+    res.status(500).json(err);
+  }
+});
+
+// 포스트 가져오기
+router.get("/:id", async (req, res) => {
+  try {
+    const message = await Message.findById(req.res.params.id);
+    res.status(200).json(message);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// 모든 포스트 가져오기
+router.get("/", async (req, res) => {
+  const nickName = req.query.nickName;
+  const message = req.query.message;
+  try {
+    messages = await Message.find();
+    res.status(200).json(messages);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
