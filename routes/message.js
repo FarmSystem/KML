2a4c4b572e298f954e9c0c495fa82eeb4386f263
gaeun <geun5744@gmail.com>
2023-01-04 19:47:46 +0900

    메시지 업데이트 추가

--- routes/message.js
+++ routes/message.js
@@ -15,7 +15,23 @@ router.post("/message", async (req, res) => {
   }
 });
 
-// 포스트 가져오기
+// 메시지 업데이트
+router.put("/message/:id", async (req, res) => {
+  try {
+    const updateMessage = await Message.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updateMessage);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// 메시지 가져오기
 router.get("/:id", async (req, res) => {
   try {
     const message = await Message.findById(req.res.params.id);
@@ -25,7 +41,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// 모든 포스트 가져오기
+// 모든 메시지 가져오기
 router.get("/", async (req, res) => {
   try {
     messages = await Message.find();
