397a5f762f4da047d7aaa166a3d794bd5ffb6e17
gaeun <geun5744@gmail.com>
2023-01-12 18:03:32 +0900

    구단 선택 라디오버튼 추가

--- client/src/pages/message/Message.jsx
+++ client/src/pages/message/Message.jsx
@@ -1,5 +1,50 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
 
 export default function Message() {
-  return <div>구단 선택 + 응원메시지 남기기</div>;
+  const [nickName, setNickName] = useState("");
+  const [message, setMessage] = useState("");
+  const [club, setClub] = useState("");
+  const [player, setPlayer] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newMessage = {
+      nickName,
+      message,
+      club,
+      player,
+    };
+    try {
+      await axios.post("/message", newMessage);
+    } catch (err) {}
+  };
+
+  return (
+    <div>
+      구단 선택 + 응원메시지 남기기
+      <form className="messageForm" onSubmit={handleSubmit}>
+        <label>
+          <input type="radio" name="club" value="롯데" onChange={(e) => setClub(e.target.value)}/>
+          <span>롯데</span>
+        </label>
+
+        <label>
+          <input type="radio" name="club" value="SSG" onChange={(e) => setClub(e.target.value)}/>
+          <span>SSG</span>
+        </label>
+
+        <label>
+          <input type="radio" name="club" value="삼성" onChange={(e) => setClub(e.target.value)}/>
+          <span>삼성</span>
+        </label>
+
+        <label>
+          <input type="radio" name="club" value="키움" onChange={(e) => setClub(e.target.value)}/>
+          <span>키움</span>
+        </label>
+      </form>
+    </div>
+  );
 }
