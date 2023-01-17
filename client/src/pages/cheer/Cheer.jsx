3fc4c6d7d4c1b4ccadf355d7fa27806a04af5758
suhyun <suhyun10201@naver.com>
2023-01-17 18:23:50 +0900

    응원페이지 꾸미기

--- client/src/pages/cheer/Cheer.jsx
+++ client/src/pages/cheer/Cheer.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './cheer.css';
 
 export default function Cheer() {
   return (
-    <div>
-      응원하기 페이지
-      <button>
+    <section className="background">
+      <div className="intro__dark">
+      <div className="intro">
+      우리 구단을 응원합니다!🙌
+      </div>
+      <div className="image">
+      <img className="img1" src=".\logos\한국가스공사.png" alt="한국가스공사"></img>
+      </div>
+      {/* 
+      <img className="img2" src=".\logos\썬더스.svg" alt="썬더스"></img>
+      <img className="img3" src=".\logos\원주DB.svg" alt="원주DB"></img>
+      <img className="img4" src=".\logos\캐럿점퍼스.png" alt="캐럿점퍼스"></img>
+      <img className="img5" src=".\logos\현대모비스.png" alt="현대모비스"></img>
+      <img className="img6" src=".\logos\kcc이글스.png" alt="KCC이글스"></img>
+      <img className="img7" src=".\logos\kgc로고.png" alt="KGC"></img>
+      <img className="img8" src=".\logos\kt소닉붐.png" alt="KT소닉붐"></img>
+      <img className="img9" src=".\logos\lg세이커스.png" alt="LG세이커스"></img>
+      <img className="img10" src=".\logos\sk나이츠.svg" alt="SK나이츠"></img> */}
+      <button className="message">
         <Link to="/message">메시지 남기기</Link>
+        
       </button>
-      <button>
-        <Link to="/parade">퍼dgdgdgdgdgdg레이드</Link>
+      <button className="parade">
+        <Link to="/parade">퍼레이드</Link>
       </button>
-    </div>
+      </div>
+      
+    </section>
   );
 }
