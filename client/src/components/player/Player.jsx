9956b07f548a6849b91095af59ba248431dcc641
gaeun <geun5744@gmail.com>
2023-01-27 10:20:08 +0900

    퍼레이드 이미지 변경

--- client/src/components/player/Player.jsx
+++ client/src/components/player/Player.jsx
@@ -1,47 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import player1 from "../player/변준형선수 이미지.png";
-import player2 from "../player/유진선수 이미지.png";
-import player3 from "../player/문성곤선수 이미지.png";
-import player4 from "../player/허웅선수 이미지.png";
-import player5 from "../player/김동량선수 이미지.png";
-import player6 from "../player/이대헌선수 이미지.png";
+import player1 from "../player/변준형 선수 이미지.png";
+import player2 from "../player/유진 선수 이미지.png";
+import player3 from "../player/문성곤 선수 이미지.png";
+import player4 from "../player/허웅 선수 이미지.png";
+import player5 from "../player/김동량 선수 이미지.png";
+import player6 from "../player/이대헌 선수 이미지.png";
+import "./player.css";
 
 export default function Player({ player }) {
   if (player.player === "변준형") {
     return (
       <Link to={`/parade/${player._id}`}>
-        <img src={player1} alt="" />
+        <img src={player1} alt="" className="img" />
       </Link>
     );
   } else if (player.player === "유진") {
     return (
       <Link to={`/parade/${player._id}`}>
-        <img src={player2} alt="" />
+        <img src={player2} alt="" className="img" />
       </Link>
     );
   } else if (player.player === "문성곤") {
     return (
       <Link to={`/parade/${player._id}`}>
-        <img src={player3} alt="" style={{ width: 200 }} />
+        <img src={player3} alt="" className="img" />
       </Link>
     );
   } else if (player.player === "허웅") {
     return (
       <Link to={`/parade/${player._id}`}>
-        <img src={player4} alt="" />
+        <img src={player4} alt="" className="img" />
       </Link>
     );
   } else if (player.player === "김동량") {
     return (
       <Link to={`/parade/${player._id}`}>
-        <img src={player5} alt="" style={{ width: 200 }} />
+        <img src={player5} alt="" className="img" />
       </Link>
     );
   } else if (player.player === "이대헌") {
     return (
       <Link to={`/parade/${player._id}`}>
-        <img src={player6} alt="" />
+        <img src={player6} alt="" className="img" />
       </Link>
     );
   }
--- client/src/components/players/Players.jsx
+++ client/src/components/players/Players.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Player from "../player/Player";
+import "./players.css";
 
 export default function Players({ players }) {
   return (
-    <div>
+    <div className="bg2">
       {players.map((p) => (
         <Player player={p} />
       ))}
