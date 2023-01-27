82b27eb27eaa588d1111dbf373c0c0a820114f47
bbabbi <mooncheayoung@gmail.com>
2023-01-27 14:35:21 +0900

    최종수정

--- client/src/pages/parade_message/Parade_message.jsx
+++ client/src/pages/parade_message/Parade_message.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import PlayerMessage from "../../components/palyer_message/Player_message";
+import "./parade_message.css";
 
 export default function Parade_message() {
   return (
     <div>
-      <PlayerMessage />
+      <div className = "bg">
+        <div className = "soqn">
+          <div className = "whatmessage">
+            <PlayerMessage />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
